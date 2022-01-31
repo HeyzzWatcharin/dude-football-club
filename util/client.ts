a5f84eb85d5b59fe58e519f0ca3a069095d29e8c
watcharin <watcharin.r@apollo21.asia>
2022-01-31 08:27:06 +0700

    add .env

--- util/client.ts
+++ util/client.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: `http://api.football-data.org`,
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
 
     timeout: 100 * 1000,
 
+    // *************************************************************
+    // Note: Just Change Key Token here.
+    // *************************************************************
     headers: {
         'X-Auth-Token': 'bb428dd4f10548debe25e5f71454529e'
     },
